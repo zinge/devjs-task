e92761b2a497e85420a808a9fba85fbbd198baea
Semyon Zingelshuher <s.a.zingelshuher@gmail.com>
2017-10-25 00:25:41 +0300

    create posts

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,30 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld/>
+    <!-- <img src="./assets/logo.png"> -->
+    <!-- <HelloWorld/> -->
+    <posts/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
+// import HelloWorld from './components/HelloWorld'
+import Posts from './components/Posts'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Posts
   }
 }
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  /* font-family: 'Avenir', Helvetica, Arial, sans-serif; */
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  /* text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 60px; */
 }
 </style>
---
+++ src/components/Posts.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="posts">
+    {{postPool}}
+  </div>
+</template>
+<script>
+import axios from 'axios'
+
+export default {
+  name: 'Posts',
+
+  data: () => ({
+    postPool: [],
+    error: []
+  }),
+
+  mounted: {
+    getAllPosts () {
+      axios.get('https://jsonplaceholder.typicode.com/posts')
+      .then(response => {
+        this.postPool = response.data
+      })
+      .catch(err => {
+        this.error.push(err)
+      })
+    }
+  }
+}
+</script>
